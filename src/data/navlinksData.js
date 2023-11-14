0a42caed03e116c78c2515675f70ece57dcb35cc
Léo Lecée <leo.lecee@gmail.com>
2023-11-14 09:47:20 +0700

    Revamp hero section

---
+++ src/data/navlinksData.js
@@ -0,0 +1,24 @@
+const navLinksData = [
+  {
+    text: "Home",
+    path: "/",
+  },
+  {
+    text: "About",
+    path: "/#about",
+  },
+  {
+    text: "Skills",
+    path: "/#skills",
+  },
+  {
+    text: "Works",
+    path: "/#works",
+  },
+  {
+    text: "Contact",
+    path: "/#contact",
+  },
+];
+
+export default navLinksData;
---
+++ src/data/socialData.js
@@ -0,0 +1,20 @@
+const socialData = [
+  {
+    name: "Github",
+    href: "https://www.github.com",
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/",
+  },
+];
+
+export default socialData;
