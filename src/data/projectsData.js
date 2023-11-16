472677669216a87ceb5878eb7e74e5212454ee90
Léo Lecée <leo.lecee@gmail.com>
2023-11-16 12:03:31 +0700

    Projects Data + Carousel start

---
+++ src/data/projectsData.js
@@ -0,0 +1,42 @@
+import Coffee from "../images/Coffee.jpg";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quibusdam soluta recusandae laboriosam. Quod libero inventore ducimus eius aliquam repellat blanditiis, praesentium incidunt delectus, fuga dolore maiores dolorum, omnis minus.",
+    technologies: ["Javascript", "React", "CSS"],
+    client: "OpenClassrooms",
+    source: Coffee,
+    name: "Project 1",
+    github: "",
+    website: "",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi quis tincidunt luctus, urna nunc accumsan felis, id tincidunt nisl justo vitae libero. Donec nec dolor vitae nunc tempus luctus. Ut vitae lectus euismod, aliquet nisi quis, ultricies quam. Donec auctor, nisi quis tincidunt luctus, urna nunc accumsan felis, id tincidunt nisl justo vitae libero. Donec nec dolor vitae nunc tempus luctus. Ut vitae lectus euismod, aliquet nisi quis, ultricies quam.",
+    technologies: ["Javascript", "React", "CSS"],
+    client: "OpenClassrooms",
+    source: Coffee,
+    name: "Project 2",
+    github: "",
+    website: "",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi quis tincidunt luctus, urna nunc accumsan felis, id tincidunt nisl justo vitae libero. Donec nec dolor vitae nunc tempus luctus. Ut vitae lectus euismod, aliquet nisi quis, ultricies quam. Donec auctor, nisi quis tincidunt luctus, urna nunc accumsan felis, id tincidunt nisl justo vitae libero. Donec nec dolor vitae nunc tempus luctus. Ut vitae lectus euismod, aliquet nisi quis, ultricies quam.",
+    technologies: ["Javascript", "React", "CSS"],
+    client: "OpenClassrooms",
+    source: Coffee,
+    name: "Project 3",
+    github: "",
+    website: "",
+  },
+];
+
+export default projectsData;
