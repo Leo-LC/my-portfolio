e602d660bea2b359be1c76b7fce869a1b4ad4469
Léo Lecée <leo.lecee@gmail.com>
2023-11-20 20:35:35 +0700

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
+  /*   output: "server", */
   site: "https://leo-lc.github.io",
   base: "/my-portfolio",
 });
