8b6f85c599c4c5a1ac6ab8472b6210a9c7491c8f
Léo Lecée <leo.lecee@gmail.com>
2023-11-22 21:29:49 +0700

    Add node adapter + build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  /*   output: "server", */
+  output: "hybrid",
   /*   site: "https://leo-lc.github.io",
   base: "/my-portfolio", */
+
+  build: {
+    format: "file"
+  },
+  adapter: node({
+    mode: "standalone"
+  })
 });
