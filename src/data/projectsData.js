8e7cff55dab5903c57648bbb25ae7f1769bf531e
Léo Lecée <leo.lecee@gmail.com>
2023-11-23 15:47:01 +0700

    Added projects data + Readme.md to projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,52 +1,53 @@
-import Coffee from "../images/Coffee.jpg";
+import ArgentBankLogo from "../images/ArgentBankLogo.png";
 
 const projectsData = [
   {
     id: 1,
-    title: "Nina Carducci",
-    description:
-      "Le challenge était d'optimiser le site de la photographe Nina Carducci. Il a fallu reprendre l'ensemble de ses photos et les optimiser pour le web. J'ai également pris le temps de réaliser un référencement local afin d'améliorer sa visibilité auprès de clients potentiels. Amélioration du SEO.",
-    technologies: ["React", "CSS", "Lighthouse", "Wave"],
-    client: "OpenClassrooms",
-    source: Coffee,
-    name: "Project 9",
-    github: "https://github.com/Leo-LC/projet_09",
+    title: "Argent Bank",
+    tags: ["API", "React", "Redux", "Swagger"],
+    source: ArgentBankLogo,
+    github: "https://github.com/Leo-LC/Argent-Bank/blob/main/README.md",
     website: null,
   },
   {
     id: 2,
-    title: "Debug",
-    description:
-      "Le client possédait déjà un site de présentation de ses projets. En revance, celui-ci comportait plusieurs soucis et 'bugs' techniques qu'il a fallu identifier et corriger",
-    technologies: ["React", "Devtools", "Something"],
-    client: "OpenClassrooms",
-    source: Coffee,
-    name: "Project 2",
-    github: "https://github.com/Leo-LC/projet_10",
+    title: "724Events",
+    tags: ["Debugging", "React devtools", "Jest"],
+    source: ArgentBankLogo,
+    github: "https://github.com/Leo-LC/724-Events/blob/main/README.md",
     website: null,
   },
   {
     id: 3,
-    title: "Argent Bank",
-    description:
-      "Double challenge ici. Dans un premier temps, se servir des wireframes et de la base de donneé existante pour réaliser les appels à l'API pour les requêtes stipulées par le client. Dans un second temps, il a fallu créer des routes API pour de futures fonctionnalités en respectant les principes existants.",
-    technologies: ["JS", "React", "CSS"],
-    client: "OpenClassrooms",
-    source: Coffee,
-    name: "Project 3",
-    github: "https://github.com/Leo-LC/projet_11",
+    title: "Nina Carducci",
+    tags: ["Accessibility", "SEO", "Performances", "Social Metas"],
+    source: ArgentBankLogo,
+    github: "https://github.com/Leo-LC/Nina-Carducci/blob/main/README.md",
+    website: null,
+  },
+
+  {
+    id: 4,
+    title: "Kasa",
+    tags: ["Web App", "React", "Responsive"],
+    source: ArgentBankLogo,
+    github: "https://github.com/Leo-LC/Kasa/blob/main/README.md",
     website: null,
   },
   {
-    id: 4,
-    title: "Portfolioception",
-    description: "Mon portfolio personnel sur lequel vous naviguez",
-    technologies: ["Astro", "Lenis", "JS"],
-    client: "Moi-même",
-    source: Coffee,
-    name: "Project 4",
-    github: "",
-    website: null,
+    id: 5,
+    title: "Sophie Bluel",
+    tags: ["Portfolio", "WebApp", "Sass", "Database"],
+    source: ArgentBankLogo,
+    github: "https://github.com/Leo-LC/Sophie-Bluel/blob/master/README.md",
+  },
+
+  {
+    id: 6,
+    title: "Ohmyfood",
+    tags: ["Web App", "Mobile-First", "Responsive", "Animations"],
+    source: ArgentBankLogo,
+    github: "https://github.com/Leo-LC/Ohmyfood/blob/main/README.md",
   },
 ];
 
